627a32013ca10ae152c66598b5bb4bdd9d6e8048
Daris Fikri Zhalifunnas <127469235+Ardaza@users.noreply.github.com>
2024-06-22 23:21:21 +0700

    fix build

--- Assets/Puzzle/CustPropertyDrawer.cs
+++ Assets/Puzzle/CustPropertyDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
@@ -39,3 +40,4 @@ public class CustPropertyDrawer : PropertyDrawer
 		return 18f * 10;
 	}
 }
+#endif
